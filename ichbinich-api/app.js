8d1a45beda7d77b598cd7a60cf2c0b6eff7628d3
remarkies <luka@ichbinich.ch>
2020-10-18 18:45:01 +0200

    added paintings get request listener

--- ichbinich-api/app.js
+++ ichbinich-api/app.js
@@ -8,14 +8,28 @@ database.connect().then(function () {
     console.log('connected');
 });
 
-app.get('/', (req, res) => {
+let paintings = require('./routes/paintings');
+app.use('/paintings', paintings);
 
-    database.query("select * from employee;", null).then((output) => {
-        console.log(output);
-    });
-    res.send('Hello World!')
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json(err);
+    console.log(err);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Listening at http://localhost:${port}`)
 })
---
+++ ichbinich-api/routes/paintings.js
@@ -0,0 +1,62 @@
+let express = require('express');
+let database = require('../services/database');
+let router = express.Router();
+
+// return json object with all paintings
+router.get('/',function(request,response){
+
+    // get paintings from database
+    database.query("SELECT P.ID,\n" +
+        "       P.name,\n" +
+        "       P.style_id,\n" +
+        "       S.description 'STYLE',\n" +
+        "       p.technique_id,\n" +
+        "       t.description 'TECHNIQUE',\n" +
+        "       P.underground_id,\n" +
+        "       U.description 'UNDERGROUND',\n" +
+        "       P.height,\n" +
+        "       P.width,\n" +
+        "       P.depth,\n" +
+        "       P.price,\n" +
+        "       P.collection_id,\n" +
+        "       C.name        'COLLECTION',\n" +
+        "       P.series_id,\n" +
+        "       SE.name       'SERIES'\n" +
+        "FROM PAINTING P\n" +
+        "         JOIN style S ON P.style_id = S.id\n" +
+        "         JOIN technique t on P.technique_id = t.id\n" +
+        "         join underground u on P.underground_id = u.id\n" +
+        "         LEFT JOIN collection C ON P.collection_id = C.id\n" +
+        "         LEFT JOIN series SE ON P.series_id = SE.id;", null).then((output) => {
+
+        let promises = [];
+
+        // create promises for getting all images foreach painting
+        // includes images in json object of paintings
+        output.forEach((painting) => {
+            promises.push(new Promise((resolve, reject) => {
+                database.query("SELECT I.path\n" +
+                    "FROM painting_image PI\n" +
+                    "         JOIN IMAGE I ON PI.image_id = I.id\n" +
+                    "WHERE PI.painting_id = ?;", [painting.ID]).then((paths) => {
+                    output.find(x => x.ID === painting.ID).paths = paths;
+                    resolve();
+                }).catch((err) => {
+                    reject(err);
+                });
+            }));
+        });
+
+        // execute promises
+        // then send response with json
+        Promise.all(promises).then(() => {
+            response.send(output)
+        });
+
+    }).catch((err) => {
+        console.log(err);
+        response.send('BACKEND ERROR. CODE 0001')
+    });
+});
+
+module.exports = router;
